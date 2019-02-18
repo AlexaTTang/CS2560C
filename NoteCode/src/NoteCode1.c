2df4b37f94d3be98b807c4252ba7300020e53a3f
Alexa Tang <alexatang@cpp.edu>
2019-02-17 21:40:14 -0800

    16th commit, Lecture 6, #4

--- NoteCode/src/NoteCode1.c
+++ NoteCode/src/NoteCode1.c
@@ -4,21 +4,22 @@
  *  Created on: Feb 17, 2019
  *      Author: alexa
  */
-//STRING
-//READ/WRITE using get and put
-#include <stdio.h>
-int main( ) {
+//You define type of I/O
+//scanf and printf
 
-   char str[100];       //char array STRING used as buffer
+//reading two items a string and int
+#include <stdio.h>
+int main( )
+{
+
+   char str[100];
+   int i;
 
    printf( "Enter a value :");
    fflush(stdout);  //ECLIPSE BUG
-   gets( str );
+   scanf("%s %d", str, &i);
 
-   printf( "\nYou entered: ");
-   puts( str );
+   printf( "\nYou entered: %s %d ", str, i);
 
    return 0;
 }
-
-
