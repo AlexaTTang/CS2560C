5d7c858fb57fb4dd8d28a30d5e6918330390b53f
Alexa Tang <alexatang@cpp.edu>
2019-02-17 20:31:03 -0800

    Fifth commit, Arrays

--- NoteCode/src/NoteCode1.c
+++ NoteCode/src/NoteCode1.c
@@ -4,34 +4,38 @@
  *  Created on: Feb 17, 2019
  *      Author: alexa
  */
-#include<stdio.h>
+//Arrays -- allocate one contiguous block of memory holding declaring multiple of one data type
+//name of array used is an address of memory.
 
-// Global variables
-int A;
-int B;
+//Declaration with Assignment -- declaring 10 doubles
+double balance[10] = {1000.0, 2.0, 3.4, 7.0, 50.0};
 
-int fun()
-{
-  static int count = 0; //Static Variable
-  count++;
-  return count;
+//when using in calculation or assignment below recall always start at 0
+//balance[0] = 1000.0 , balance[3] = 7.0, balance[7] = 0, balance[10] = error out of bounds
+
+//Also acceptable because size will be known. Size will be 5 hence --> double balance[5]
+//double balance[] = {1000.0, 2.0, 3.4, 7.0, 50.0};
+
+//code example
+#include <stdio.h>
+
+int main () {
+
+   int n[ 10 ]; /* n is an array of 10 integers */
+   int i,j;
+
+   /* initialize elements of array n to 0 */
+   for ( i = 0; i < 10; i++ )
+   {
+      n[ i ] = i + 100; /* set element at location i to i + 100 */
+   }
+
+   /* output each array element's value */
+   for (j = 0; j < 10; j++ )
+   {
+      printf("Element[%d] = %d\n", j, n[j] );
+   }
+
+   return 0;
 }
-
-int Add()
-{
-    return A + B;
-}
-
-int main()
-{
-    int answer; // Local variable
-    A = 5;
-    B = 7;
-    answer = Add();
-    printf("%d\n",answer);
-    printf("%d ", fun());
-    printf("%d ", fun());
-    return 0;
-}
-
 
