6db155b1f4be8fa21f463dccb6e7d47dfe9397c1
Alexa Tang <alexatang@cpp.edu>
2019-02-17 18:10:57 -0800

    Fourth Commit, Global and Static variables

--- NoteCode/src/NoteCode1.c
+++ NoteCode/src/NoteCode1.c
@@ -4,33 +4,34 @@
  *  Created on: Feb 17, 2019
  *      Author: alexa
  */
-#include <stdio.h>
+#include<stdio.h>
+
+// Global variables
+int A;
+int B;
+
+int fun()
+{
+  static int count = 0; //Static Variable
+  count++;
+  return count;
+}
+
+int Add()
+{
+    return A + B;
+}
+
 int main()
 {
-
-	/* local variable definition */
-	   char grade = 'B';
-	   switch(grade)
-	   {
-	      case 'A' :
-	         printf("Excellent!\n" );
-	         break;
-	      case 'B' :
-	      case 'C' :
-	         printf("Well done\n" );
-	         break;
-	      case 'D' :
-	         printf("You passed\n" );
-	         break;
-	      case 'F' :
-	         printf("Better try again\n" );
-	         break;
-	      default :
-	         printf("Invalid grade\n" );
-	   }
-
-	   printf("Your grade is  %c\n", grade );
-
-	   return 0;
+    int answer; // Local variable
+    A = 5;
+    B = 7;
+    answer = Add();
+    printf("%d\n",answer);
+    printf("%d ", fun());
+    printf("%d ", fun());
+    return 0;
 }
+
 
