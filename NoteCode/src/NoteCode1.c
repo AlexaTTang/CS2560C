f4f2b64e9f76f6d7348dbe1273a00a2b062cce25
Alexa Tang <alexatang@cpp.edu>
2019-02-17 18:05:37 -0800

    Third commit, Switch statement

--- NoteCode/src/NoteCode1.c
+++ NoteCode/src/NoteCode1.c
@@ -8,10 +8,29 @@
 int main()
 {
 
-	 int a , b;
-	   a = 10;
-	   printf( "Value of b is %d\n", (a == 1) ? 20: 30 );
-	   printf( "Value of b is %d\n", (a == 10) ? 20: 30 );
+	/* local variable definition */
+	   char grade = 'B';
+	   switch(grade)
+	   {
+	      case 'A' :
+	         printf("Excellent!\n" );
+	         break;
+	      case 'B' :
+	      case 'C' :
+	         printf("Well done\n" );
+	         break;
+	      case 'D' :
+	         printf("You passed\n" );
+	         break;
+	      case 'F' :
+	         printf("Better try again\n" );
+	         break;
+	      default :
+	         printf("Invalid grade\n" );
+	   }
+
+	   printf("Your grade is  %c\n", grade );
+
 	   return 0;
 }
 
